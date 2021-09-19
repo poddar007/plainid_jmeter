62e8d7874a34d12b5eeed1d1adc4d6885371d873
Amit Poddar <amitpoddark@gmail.com>
2021-09-18 20:42:56 -0400

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -12,6 +12,6 @@ echo "Starting JMeter tests on ${URL}"
 
 #Command
 cd ../../bin
-sh jmeter -n -t /opt/apache-jmeter-5.2.1/code/plainid_jmeter/PlainID Users.jmx -l /opt/apache-jmeter-5.4.1/code/test_output.csv -JThreadNumber=4 -JRampUpPeriod=10
+sh jmeter -n -t /opt/apache-jmeter-5.2.1/code/plainid_jmeter/PlainID Users.jmx -l /opt/apache-jmeter-5.4.1/results/result.csv -JThreadNumber=4 -JRampUpPeriod=10
 
 echo "********entry.sh file RAN SUCCESSFULLY*******"
